75953f030785feaf0c46d990e19d11f8facd99bd
SCIENTIST MUTURI <muturijoseph064@gmail.com>
2023-07-13 15:47:18 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import LogIn from './context/LogIn';
+import MoviePage from './context/MoviePage';
+import UserContext from './context/UserContext';
+import CartContext from './context/CartContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    currentUser:{name:''}
+  };
+  handleLoggedIn = userName =>{
+    console.log('Getting user: ' + userName);
+    const user = {name:'Scientist Muturi'};
+    this.setState({currentUser:user});
+
+  };
+  render() {
+    return (
+      <CartContext.Provider value={{cart :['Muturi','Joseph']}}>
+      <UserContext.Provider 
+         value={{
+           currentUser:this.state.currentUser,
+           onLoggedIn:this.handleLoggedIn
+         }}>
+       <div>
+        <MoviePage/>
+        <LogIn/>
+       </div>
+      </UserContext.Provider>
+      </CartContext.Provider>
+    );
+  }
 }
 
 export default App;
